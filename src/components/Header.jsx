7afb1abba6862a4fd773926df1411fcb8346f4f5
FernandaRubio74 <20235795@esen.edu.sv>
2025-07-07 20:11:18 -0600

    - Menu, events and banner of home - new dependencies (for ui)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Header = () => {
     return (
         <>
-            <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-800 via-violet-950 to-stone-900 py-24 sm:py-32">
+            <Navbar />
+            <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-800 via-violet-950 to-stone-900 py-20 sm:py-32">
                 <div
                     aria-hidden="true"
                     className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
@@ -12,6 +14,7 @@ const Header = () => {
                         className="relative bg-gradient-to-b  via-violet-950 to-stone-900 w-96 h-96"
                     />
                 </div>
+
                 <div
                     aria-hidden="true"
                     className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
@@ -23,8 +26,15 @@ const Header = () => {
                         }}
                         className="relative bg-gradient-to-b to-stone-900 w-[72rem] h-[72rem]"
                     />
+
+
                 </div>
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8 relative">
+                    {/* Imagen abajo a la derecha, podemos cambiarla luego por una del logo esen (maybe) */}
+                    <div className="absolute inset-yp-2 right-0 hidden lg:block">
+                        <img src="weekuplogo.png" alt="" className="w-100 h-90 object-contain" />
+                    </div>
+
                     <div className="mx-auto max-w-2xl lg:mx-0">
                         <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">Know all the events!</h2>
                         <p className="mt-8 text-lg font-medium text-pretty text-gray-300 sm:text-xl/8">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import Events from '../components/Events'
 
 const Home = () => {
     return (
         <>
         <Header/>
+        <Events/>
         </>
     )
 }
